6b63da121f37c1724094075408a77a20a3323c68
EdilLori <edillori07@gmail.com>
2024-08-01 09:48:21 +0200

    Add files via upload

--- public/galleria.js
+++ public/galleria.js
@@ -7,27 +7,23 @@ document.addEventListener("DOMContentLoaded", function() {
     clonedImages.forEach(img => gallery.appendChild(img));
 
     let index = 0;
-    const slideWidth = images[0].clientWidth + parseInt(getComputedStyle(images[0]).marginRight); // Larghezza immagine più margine
+    const slideWidth = images[0].clientWidth + 30; // Larghezza immagine più margine
     const intervalTime = 3000; // Tempo in millisecondi per ogni slide
 
     function nextSlide() {
         index++;
 
         // Calcola la nuova trasformazione per lo scorrimento
-        const transformValue = `translateX(-${index * slideWidth}px)`;
+        const transformValue = `translateX(-${index * (slideWidth - 10)}px)`;
 
         // Applica la trasformazione con una transizione fluida
         gallery.style.transition = 'transform 1s ease';
-        gallery.style.webkitTransform = transformValue; // Prefisso per WebKit
-        gallery.style.MozTransform = transformValue;   // Prefisso per Firefox
         gallery.style.transform = transformValue;
 
         // Resetta l'indice e la trasformazione dopo aver mostrato le immagini duplicate
         if (index >= images.length) {
             setTimeout(() => {
                 gallery.style.transition = 'none';
-                gallery.style.webkitTransform = 'translateX(0)';
-                gallery.style.MozTransform = 'translateX(0)';
                 gallery.style.transform = 'translateX(0)';
                 index = 0;
             }, 1000); // Dopo un secondo, ripristina la posizione senza transizione
